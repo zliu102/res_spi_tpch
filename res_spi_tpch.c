1ef647ee1b8eaa5f1434a79a90d07eb558f31065
zliu102 <zliu102@hawk.iit.edu>
2023-04-07 21:03:20 -0500

    a

--- res_spi_tpch.c
+++ res_spi_tpch.c
@@ -323,9 +323,9 @@ reservoir_sampler_tpch(PG_FUNCTION_ARGS)
 
     //tupdesc = SPI_tuptable->tupdesc;
     tupdesc = CreateTemplateTupleDesc(3, false);
-    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_suppkey", INT4OID, -1, 0);
-    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_linenumber ", INT4OID, -1, 0);
     TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_orderkey", INT4OID, -1, 0);
+    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_discount ", NUMERICOID, -1, 0);
+    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_extendedprice", NUMERICOID, -1, 0);
     oldcontext = MemoryContextSwitchTo(rsinfo->econtext->ecxt_per_query_memory);
     tupstore = tuplestore_begin_heap(true, false, work_mem);
     rsinfo->setResult = tupstore;
@@ -340,8 +340,8 @@ reservoir_sampler_tpch(PG_FUNCTION_ARGS)
     bool initialized = false;
     for(row = 0; row < SPI_processed; row++){
      
-        int attnum1 = SPI_fnumber(SPI_tuptable->tupdesc, "l_suppkey");
-        int attnum2 = SPI_fnumber(SPI_tuptable->tupdesc, "l_linenumber");
+        int attnum1 = SPI_fnumber(SPI_tuptable->tupdesc, "l_orderkey");
+        int attnum2 = SPI_fnumber(SPI_tuptable->tupdesc, "l_discount");
         char* value1 = SPI_getvalue((SPI_tuptable->vals)[row], SPI_tuptable->tupdesc, attnum1);
         char* value2 = SPI_getvalue((SPI_tuptable->vals)[row], SPI_tuptable->tupdesc, attnum2);
         char *current_group = strcat(value1, ",");
