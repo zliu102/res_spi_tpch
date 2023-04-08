6758a29f0808cfb5d08d5c00ae33f6130a9d5b3b
zliu102 <zliu102@hawk.iit.edu>
2023-04-08 00:56:21 -0500

    a

--- res_spi_tpch.c
+++ res_spi_tpch.c
@@ -324,8 +324,8 @@ reservoir_sampler_tpch(PG_FUNCTION_ARGS)
     //tupdesc = SPI_tuptable->tupdesc;
     tupdesc = CreateTemplateTupleDesc(3, false);
     TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_suppkey", INT4OID, -1, 0);
-    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_quantity", NUMERICOID, -1, 0);
     TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_tax", NUMERICOID, -1, 0);
+    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_quantity", NUMERICOID, -1, 0);
     oldcontext = MemoryContextSwitchTo(rsinfo->econtext->ecxt_per_query_memory);
     tupstore = tuplestore_begin_heap(true, false, work_mem);
     rsinfo->setResult = tupstore;
@@ -341,7 +341,7 @@ reservoir_sampler_tpch(PG_FUNCTION_ARGS)
     for(row = 0; row < SPI_processed; row++){
      
         int attnum1 = SPI_fnumber(SPI_tuptable->tupdesc, "l_suppkey");
-        int attnum2 = SPI_fnumber(SPI_tuptable->tupdesc, "l_quantity");
+        int attnum2 = SPI_fnumber(SPI_tuptable->tupdesc, "l_tax");
         char* value1 = SPI_getvalue((SPI_tuptable->vals)[row], SPI_tuptable->tupdesc, attnum1);
         char* value2 = SPI_getvalue((SPI_tuptable->vals)[row], SPI_tuptable->tupdesc, attnum2);
         char *current_group = strcat(value1, ",");
