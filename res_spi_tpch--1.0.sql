129daa3bccd6e0502a66b5c67ec8d1336b9adde1
zliu102 <zliu102@hawk.iit.edu>
2023-04-07 21:09:08 -0500

    a

--- res_spi_tpch--1.0.sql
+++ res_spi_tpch--1.0.sql
@@ -31,7 +31,7 @@ LANGUAGE C STRICT;
 
 
 CREATE FUNCTION reservoir_sampler_tpch(sampleSize bigint, tablename text, otherAttribue text,groupby text) 
-RETURNS TABLE (b int, c int, a int) 
+RETURNS TABLE (b int, c numeric, a numeric) 
 AS 'MODULE_PATHNAME','reservoir_sampler_tpch' 
 LANGUAGE C STRICT;
 
